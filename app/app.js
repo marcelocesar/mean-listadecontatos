48991183efebbe78ee3b0a765b82842dbb233bf6
Marcelo Cesar <marcelop3@gmail.com>
2016-06-22 23:43:44 -0300

    add new contact

--- app/app.js
+++ app/app.js
@@ -1,25 +1,53 @@
-angular.module('app',['ngRoute'])
+angular.module('app', ['ngRoute'])
   .config(function ($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl: "views/list.html",
-      controller: "ListController",
-      resolve: {
-        contacts: function(Contacts) {
-          return Contacts.getContacts();
+    $routeProvider
+      .when('/', {
+        templateUrl: "views/list.html",
+        controller: "ListController",
+        resolve: {
+          contacts: function (ServiceContacts) {
+            return ServiceContacts.getContacts();
+          }
         }
-      }
-    })
+      })
+      .when("/new-contact", {
+        controller: "NewContactController",
+        templateUrl: "views/new-contact.html"
+      })
+      .otherwise({redirectTo: '/'});
   })
-  .service("Contacts", function($http) {
-    this.getContacts = function() {
-      return $http.get("/contacts").
-      then(function(response) {
+  .service("ServiceContacts", function ($http) {
+
+    this.getContacts = function () {
+      return $http.get("/contacts").then(function (response) {
         return response;
-      }, function(response) {
+      }, function (response) {
         alert("Error retrieving contacts.");
       });
-    }
+    };
+
+    this.createContact = function (contact) {
+      return $http.post('/contacts', contact).then(function (resp) {
+        return resp;
+      }, function (resp) {
+        console.log("Erro não foi possivel criar o contato.");
+      });
+    };
   })
-  .controller("ListController", function(contacts, $scope) {
+  .controller("ListController", function (contacts, $scope) {
     $scope.contacts = contacts.data;
-  });
+  })
+  .controller("NewContactController", function ($scope, ServiceContacts) {
+    
+    $scope.newContact = function (contact) {
+      ServiceContacts.createContact(contact)
+        .then(function (resp) {
+          console.log('contato registrado com sucesso!');
+        }, function (resp) {
+          console.log('Erro na inclusao!');
+        })
+    }
+    
+  })
+
+;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
-
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
@@ -14,7 +13,7 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect('mongodb://localhost:27017/'+ CONTACTS_COLLECTION, function (err, database) {
+mongodb.MongoClient.connect('mongodb://'$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/*'mongodb://localhost/contactlist'*/, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -22,90 +21,50 @@ mongodb.MongoClient.connect('mongodb://localhost:27017/'+ CONTACTS_COLLECTION, f
 
   // Save database object from the callback for reuse.
   db = database;
-  console.log("Database connection ready");
+  console.log("Database conectado!");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen($OPENSHIFT_MONGODB_DB_PORT /*process.env.PORT */ || 8080, function () {
     var port = server.address().port;
-    console.log("App now running on port", port);
+    console.log("App funcionando na porta", port);
   });
 });
 
-// CONTACTS API ROUTES BELOW
+// RestFul API - Lista de contatos
 
-// Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
 
 /*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
+ *    GET: listar todos os contatos
+ *    POST: novo contato
  */
 
-app.get("/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/contacts", function (req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
     if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
+      handleError(res, err.message, "Error get contatos");
     } else {
       res.status(200).json(docs);
     }
   });
 });
 
-app.post("/contacts", function(req, res) {
+app.post("/contacts", function (req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
 
   if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+    handleError(res, "Campos obrigatorios", "Preencher nome ou sobrenome.", 400);
   }
 
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
+      handleError(res, err.message, "Erro ao criar o novo contato.");
     } else {
       res.status(201).json(doc.ops[0]);
-    }
-  });
-});
-
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-app.get("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contact");
-    } else {
-      res.status(200).json(doc);
-    }
-  });
-});
-
-app.put("/contacts/:id", function(req, res) {
-  var updateDoc = req.body;
-  delete updateDoc._id;
-
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
-
-app.delete("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete contact");
-    } else {
-      res.status(204).end();
     }
   });
 });
